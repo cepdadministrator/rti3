1085efd6cc018c64795f4461c56679daa2a1ecda
matt jiggins <git@jiggins.ca>
2014-02-10 00:11:56 +0100

    nothing major

--- site/snippets/menu.php
+++ site/snippets/menu.php
@@ -3,13 +3,13 @@
 		<!-- Brand and toggle get grouped for better mobile display -->
 		<div class="navbar-header">
 			<!-- <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse"><i class="fa fa-bars"></i></button> -->
-			<button id="trigger-overlay" type="button" class="navbar-toggle visible-xs visible-sm btn btn-default pull-right"><i class="fa fa-bars"></i></button>
+			<button id="trigger-overlay" type="button" class="navbar-toggle visible-xs btn btn-default pull-right"><i class="fa fa-bars"></i></button>
 			<a class="navbar-brand" href="<?php echo url('home') ?>"><img src="<?php echo url('assets/icons/RTi3-logo.png') ?>" alt="" /></a>
 		</div>
 
 		<!-- Collect the nav links, forms, and other content for toggling -->
 		<div class="collapse navbar-collapse navbar-ex1-collapse">
-			<ul class="nav navbar-nav">
+			<ul class="nav pull-right navbar-nav">
 			    <?php foreach($pages->visible() AS $p): ?>
 				    <li <?php echo ($p->isOpen()) ? ' class="active"' : '' ?>><a href="<?php echo $p->url() ?>"><?php echo html($p->title()) ?></a></li>
 			    <?php endforeach ?>
