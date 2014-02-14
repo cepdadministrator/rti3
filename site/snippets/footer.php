92a1db282c5fceaa9c37a52b6a864fa6e5739f36
matt jiggins <git@jiggins.ca>
2014-02-14 23:39:08 +0100

    gau

--- site/snippets/footer.php
+++ site/snippets/footer.php
@@ -63,6 +63,20 @@
 			});
 		});
 	</script>
+	
+	<script type="text/javascript">
+	  var _gauges = _gauges || [];
+	  (function() {
+	    var t   = document.createElement('script');
+	    t.type  = 'text/javascript';
+	    t.async = true;
+	    t.id    = 'gauges-tracker';
+	    t.setAttribute('data-site-id', '52fe9ae692c6ac4bad002f37');
+	    t.src = '//secure.gaug.es/track.js';
+	    var s = document.getElementsByTagName('script')[0];
+	    s.parentNode.insertBefore(t, s);
+	  })();
+	</script>
 </body>
 
 </html>
